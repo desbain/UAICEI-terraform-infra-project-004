49f02be7c073c8bd25216cbad351a20404448da8
gewa11281 <desbaina@yahoo.com>
2026-03-13 03:49:28 -0700

    created VPC in vpc module

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "tfstate-remote-backend-00014"
+    key            = "jupiter/statefile"
+    region         = "us-east-2"
+    dynamodb_table = "jupiter-state-locking-00014"
+    encrypt        = true
+  }
+}
+
+module "vpc" {
+  source         = "./vpc"
+  vpc_cidr_block = var.vpc_cidr_block
+  tags           = local.project_tags
+}
---
+++ vpc/variable.tf
@@ -0,0 +1,6 @@
+variable "vpc_cidr_block" {
+  type = string
+}
+variable "tags" {
+  type = map(string)
+}
