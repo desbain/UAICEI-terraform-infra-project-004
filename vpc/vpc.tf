16968abe8e681671bfb7b7dd23322459b772c7ad
gewa11281 <desbaina@yahoo.com>
2026-03-16 23:45:39 -0700

    deleted some scripts

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -59,143 +59,20 @@ resource "aws_subnet" "private_subnet_az_2a" {
   }
 
 #CREATING DATABASE SUBNET------------------------------------------------------------------------------------------
-  resource "aws_subnet" "db-subnet_az_2a" {
+  resource "aws_subnet" "db_subnet_az_2a" {
   vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = var.db_cider_block[0]
+  cidr_block = var.db_cidr_block[0]
   availability_zone = var.availability_zone[0]
  tags = merge(var.tags, {
     Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-db-subnet-az-2a"
   })
   }
 
-  resource "aws_subnet" "db-subnet_az_2b" {
+  resource "aws_subnet" "db_subnet_az_2b" {
   vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = var.db_cider_block[1]
+  cidr_block = var.db_cidr_block[1]
   availability_zone = var.availability_zone[1]
  tags = merge(var.tags, {
     Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-db-subnet-az-2b"
   })
   }
-
-  #CREATING PUBLIC ROUTE TABLE-----------------------------------------------------------------------------
-  resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-  
-  tags = merge(var.tags, {
-    Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-public-rt"
-  })
-}
-
-#CREATING SUBNET ASSOCIATION FOR PUBLIC SUBNETS
-resource "aws_route_table_association" "rt_ass_public_subnet_az_2a" {
-  subnet_id      = aws_subnet.db-subnet_az_2a.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "rt_ass_public_subnet_az_2b" {
-  subnet_id      = aws_subnet.db-subnet_az_2b.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-# CREATING ELASTIC IP FOR NAT GATEWAY ----------------------------------------------
-resource "aws_eip" "eip_az_2a" {
-  domain   = "vpc"
-
- tags = merge(var.tags, {
-    Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-eip-az-2a"
-  })
-}
-
-#CREATING NAT GATEWAY
-resource "aws_nat_gateway" "nat_gw_az_2a" {
-  allocation_id = aws_eip.eip_az_2a.id
-  subnet_id     = aws_subnet.public_subnet_az_2a.id
-
- tags = merge(var.tags, {
-    Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-nat-gw-az-2a"
-  })
-
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_eip.eip_az_2a, aws_subnet.public_subnet_az_2a]
-}
-
-# CREATING PRIVATE ROUTE TABLE FOR AZ 2A
-resource "aws_route_table" "private_rt_az_2a" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw_az_2a.id
-  }
-  
-  tags = merge(var.tags, {
-    Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-private-rt-az-2a"
-  })
-
-}
-
-#CREATING SUBNET ASSOCIATION FOR PRIVATE ROUTE TABLE IN AZ 2A------------------------------------------------
-resource "aws_route_table_association" "rt_ass_private_subnet_az_2a" {
-  subnet_id      = aws_subnet.private_subnet_az_2a.id
-  route_table_id = aws_route_table.private_rt_az_2a.id
-}
-
-resource "aws_route_table_association" "rt_ass_db_subnet_az_2a" {
-  subnet_id      = aws_subnet.db-subnet_az_2a.id
-  route_table_id = aws_route_table.private_rt_az_2a.id
-}
-
-# CREATING ELASTIC IP FOR NAT GATEWAY AZ 2B----------------------------------------------
-resource "aws_eip" "eip_az_2b" {
-  domain   = "vpc"
-
- tags = merge(var.tags, {
-    Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-eip-az-2b"
-  })
-}
-
-#CREATING NAT GATEWAY
-resource "aws_nat_gateway" "nat_gw_az_2b" {
-  allocation_id = aws_eip.eip_az_2b.id
-  subnet_id     = aws_subnet.public_subnet_az_2b.id
-
- tags = merge(var.tags, {
-    Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-nat-gw-az-2b"
-  })
-
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_eip.eip_az_2b, aws_subnet.public_subnet_az_2b]
-}
-
-# CREATING PRIVATE ROUTE TABLE FOR AZ 2B
-resource "aws_route_table" "private_rt_az_2b" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw_az_2b.id
-  }
-  
-  tags = merge(var.tags, {
-    Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-private-rt-az-2b"
-  })
-
-}
-
-#CREATING SUBNET ASSOCIATION FOR PRIVATE ROUTE TABLE IN AZ 2A------------------------------------------------
-resource "aws_route_table_association" "rt_ass_private_subnet_az_2b" {
-  subnet_id      = aws_subnet.private_subnet_az_2b.id
-  route_table_id = aws_route_table.private_rt_az_2b.id
-}
-
-resource "aws_route_table_association" "rt_ass_db_subnet_az_2b" {
-  subnet_id      = aws_subnet.db-subnet_az_2b.id
-  route_table_id = aws_route_table.private_rt_az_2b.id
-}
