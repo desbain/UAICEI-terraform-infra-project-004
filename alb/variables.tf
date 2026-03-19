42bfcc8647964deb4854f1e0d1bea1968df4709e
gewa11281 <desbaina@yahoo.com>
2026-03-19 07:17:23 -0700

    added autoscaling and alb modules

---
+++ alb/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "public_subnet_az_2a_id" {
+  type = string
+}
+
+variable "public_subnet_az_2b_id" {
+  type = string
+}
--- variables.tf
+++ variables.tf
@@ -29,3 +29,15 @@ variable "instance_type" {
 variable "key_name" {
   type = string
 }
+
+variable "max_size" {
+  type = number
+}
+
+variable "min_size" {
+  type = number
+}
+
+variable "desired_capacity" {
+  type = number
+}
