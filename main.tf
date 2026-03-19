94797563b27618b49305982ce6468c10772b35c3
gewa11281 <desbaina@yahoo.com>
2026-03-18 19:42:12 -0700

    added private servers to ec2

--- main.tf
+++ main.tf
@@ -24,11 +24,13 @@ module "vpc" {
 }
 
 module "ec2" {
-  source        = "./ec2"
-  subnet_id     = module.vpc.subnet_id
-  vpc_id        = module.vpc.vpc_id
-  ami_id        = var.ami_id
-  key_name      = var.key_name
-  tags          = local.project_tags
-  instance_type = var.instance_type
+  source                  = "./ec2"
+  public_subnet_az_2a_id  = module.vpc.public_subnet_az_2a_id
+  vpc_id                  = module.vpc.vpc_id
+  ami_id                  = var.ami_id
+  key_name                = var.key_name
+  tags                    = local.project_tags
+  instance_type           = var.instance_type
+  private_subnet_az_2a_id = module.vpc.private_subnet_az_2a_id
+  private_subnet_az_2b_id = module.vpc.private_subnet_az_2b_id
 }
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,6 +2,14 @@ output "vpc_id" {
   value = aws_vpc.main_vpc.id
 }
 
-output "subnet_id" {
+output "public_subnet_az_2a_id"{
   value = aws_subnet.public_subnet_az_2a.id
+}
+
+output "private_subnet_az_2a_id" {
+  value = aws_subnet.private_subnet_az_2a.id
+}
+
+output "private_subnet_az_2b_id" {
+  value = aws_subnet.private_subnet_az_2b.id
 }
